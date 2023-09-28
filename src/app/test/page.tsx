26a2f028c13d60638bab5e66657f6d0bbb1d83b9
Nicholas Ray <nray@nray.dev>
2023-09-27 23:21:04 -0600

    Update

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
+import Counter from "./Counter";
 
 export default function Article() {
 	return (
-		<div><Link href={"/"}>BlahHome</Link></div>
+		<div><Link href={"/"}>BlahHome</Link>
+		<Counter/>
+		</div>
 	)
 }
